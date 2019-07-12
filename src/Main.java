c7df3d030296cefb10037fcd563f449330a64b45
Flow Arg <coutureflorence3@gmail.com>
2019-07-12 13:09:21 +0200

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -5,51 +5,45 @@ public class Main
 
     public static void main(String[] args)
     {
-        //Notre objet Scanner
         Scanner sc = new Scanner(System.in);
+        UtilsName utils = new UtilsName();
 
-        //initialisation des variables
-        double aConvertir, convertit=0;
-        char reponse=' ';
-        char mode = ' ';
+        //énumération des variables
+        double aConvertir, convertit;
+        char reponse;
+        char mode;
 
-        System.out.println("CONVERTISSEUR DEGRES CELSIUS ET DEGRES FAHRENHEIT BY FLOWARG");
-        System.out.println("------------------------------------------------------------");
-
+        //zeparti
+        utils.title();
         do{
-
-            do{//tant que reponse n'est pas O ou N
-                mode = ' ';
-                System.out.println("Choisissez le mode de conversion : ");
-                System.out.println("1 --> Convertisseur Celsius > Fahrenheit");
-                System.out.println("2 --> Convertisseur Fahrenheit > Celsius ");
+            utils.nom();
+            do{
+                System.out.println("\nChoisissez le mode de conversion : ");
+                System.out.println("1 - Convertisseur Celsius - Fahrenheit");
+                System.out.println("2 - Convertisseur Fahrenheit - Celsius ");
                 mode = sc.nextLine().charAt(0);
 
                 if(mode != '1' && mode != '2')
-                    System.out.println("Mode inconnu :/, veuillez choisir un mode existant.");
-
+                    System.out.println("\nMode inconnu :/, veuillez choisir un mode existant.");
             }while (mode != '1' && mode != '2');
 
-            //saisie de la température à convertir
-            System.out.println("Temperature a convertir :");
+            System.out.println("\nTemperature a convertir :");
             aConvertir = sc.nextDouble();
             sc.nextLine();
 
-            //Selon le mode, on calcule différemment et on affiche le résultat
             if(mode == '1'){
                 convertit = ((9.0/5.0) * aConvertir) + 32.0;
                 System.out.print(aConvertir + " °C correspond a : ");
                 System.out.println(arrondi(convertit, 2) + " °F.");
             }
-            else{
-                convertit = ((aConvertir - 32) * 5) / 9;
+            else
+                {    convertit = ((aConvertir - 32) * 5) / 9;
                 System.out.print(aConvertir + " °F correspond a : ");
                 System.out.println(arrondi(convertit, 2) + " °C.");
             }
 
-            //On invite l'utilisateur à recommencer ou à quitter
             do{
-                System.out.println("Souhaitez-vous convertir une autre temperature ?(O/N)");
+                System.out.println("\nSouhaitez-vous convertir une autre temperature ?(O/N)");
                 reponse = sc.nextLine().charAt(0);
 
                 if(reponse != 'O' && reponse != 'N')
@@ -59,10 +53,10 @@ public class Main
 
             }while(reponse != 'O' && reponse != 'N');
 
+
         }while(reponse == 'O');
 
-        System.out.println("A bientot !");
-        System.out.println("Pour aider le projet : https://github.com/FlowArg/ThermoConvertisseur  !");
+        utils.goodbye();
 
     }
 
